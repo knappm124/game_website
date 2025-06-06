edea7f18a7a0b3964867c2607dd4cd816dc110d0
knappm124 <118485646+knappm124@users.noreply.github.com>
2025-06-06 18:20:31 -0400

    updated dictionary logic further

--- js/dictionary.js
+++ js/dictionary.js
@@ -5,19 +5,15 @@ const title = document.querySelector("h2");
 const definition = document.querySelector("p");
 
 form.addEventListener("submit", () => {
-    getDefinition();
-});
-
-async function getDefinition (){
     let word = text_field.value;
     let key = API + word; //create API url by adding base and word together
-    const response = await fetch(key);
+    const response = fetch(key);
     if (response.status = 404) {
         title.innerHTML = "Sorry, word is not a valid word";
     } else {
         format(response.body);
     }
-}
+});
 
 function format(json) {
     title.innerText = "" //Clear error message if it exists
