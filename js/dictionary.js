01c0faf0a49aefdeda53c6683e06ac71dbccc5ef
knappm124 <118485646+knappm124@users.noreply.github.com>
2025-06-06 18:17:28 -0400

    continue development of dictionary logic

--- js/dictionary.js
+++ js/dictionary.js
@@ -1,22 +1,25 @@
 const API = "https://api.dictionaryapi.dev/api/v2/entries/en/";
-const button = document.querySelector("#submit");
-const text_field = document.querySelector("#define_word");
-const title = document.querySelector("#title");
-const definition = document.querySelector("#definition");
+const form = document.querySelector("form");
+const text_field = document.querySelector("input");
+const title = document.querySelector("h2");
+const definition = document.querySelector("p");
 
-button.addEventListener("submit", getDefinition());
+form.addEventListener("submit", () => {
+    getDefinition();
+});
 
 async function getDefinition (){
     let word = text_field.value;
     let key = API + word; //create API url by adding base and word together
     const response = await fetch(key);
-    if (response.message() == "Sorry pal, we couldn't find definitions for the word you were looking for.") {
-        definition.value = "Sorry, word is not a valid word";
+    if (response.status = 404) {
+        title.innerHTML = "Sorry, word is not a valid word";
     } else {
         format(response.body);
     }
 }
 
 function format(json) {
-    definition.value = "" //Clear error message if it exists
+    title.innerText = "" //Clear error message if it exists
+    const obj = JSON.parse(json);
 }
