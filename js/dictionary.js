3db83ee281f8ac85d4fe6aacc74ebf69bcddf32c
knappm124 <118485646+knappm124@users.noreply.github.com>
2025-06-07 16:46:28 -0400

    finished dictionary logic and styline

--- js/dictionary.js
+++ js/dictionary.js
@@ -2,39 +2,48 @@ const API = "https://api.dictionaryapi.dev/api/v2/entries/en/";
 const form = document.querySelector("form");
 const text_field = document.querySelector("input");
 const title = document.querySelector("h2");
-const definition = document.querySelector("p");
+const definition = document.querySelector(".definitions");
 
 function call_api() {
-    let valid = true;
     let word = text_field.value;
-    let key = API + word; //create API url by adding base and word together
-    fetch(key)
-        .then(response => {
-            if(response.status == 404) {
-                title.innerHTML = "Sorry, " + word + " is not a valid word";
-                valid = false;
-            } else {
-                return response.json;
-            }
-        })
-        .then(body => {
-            if(valid) {
-
-            }
-        })
-        .catch((error) => {
-
-        })
-    if (response.status == 404) {
-
+    definition.innerHTML = "";
+    let valid = true;
+    if (!word) {
+        title.innerHTML = "Please enter a word to look up";
     } else {
-        response = fetch(key);
-        format(response.body);
+        let key = API + word; //create API url by adding base and word together
+        fetch(key)
+            .then(response => {
+                if (response.status == 404) {
+                    title.innerHTML = "Sorry, " + word + " is not a valid word";
+                    valid = false;
+                } else {
+                    return response.json();
+                }
+            })
+            .then(body => {
+                if (valid) {
+                    let capWord = word.charAt(0).toUpperCase() + word.slice(1);
+                    title.innerHTML = capWord;
+                    for (let i = 0; i < body[0].meanings.length; i++) {
+                        let partOfSpeech = document.createElement("h3");
+                        let capSpeech = body[0].meanings[i].partOfSpeech.charAt(0).toUpperCase() + body[0].meanings[i].partOfSpeech.slice(1);
+                        partOfSpeech.innerHTML = capSpeech + ":";
+                        definition.append(partOfSpeech);
+                        let list = document.createElement("ol");
+                        for (let j = 0; j < body[0].meanings[i].definitions.length; j++) {
+                            let defineText = body[0].meanings[i].definitions[j].definition;
+                            let define = document.createElement("li");
+                            define.innerHTML = defineText;
+                            list.append(define);
+                        }
+                        definition.append(list);
+                    }
+                }
+            })
+            .catch((error) => {
+                console.log(error);
+            })
     }
     return false;
-}
-
-function format(json) {
-    title.innerHTML = "" //Clear error message if it exists
-    const obj = JSON.parse(json);
 }
